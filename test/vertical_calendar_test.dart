397e0356a141c4a7a556506ecbbe5c7a7233135e
cas van luijtelaar <cas@rvised.com>
2021-02-27 11:10:30 +0100

    initial pakage migration

--- test/vertical_calendar_test.dart
+++ test/vertical_calendar_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide DateUtils;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:paged_vertical_calendar/paged_vertical_calendar.dart';
 import 'package:paged_vertical_calendar/utils/date_utils.dart';
