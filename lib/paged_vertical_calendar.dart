6050ea28056b221b80d23874dbd100f6cc4b6cfe
desmeit <post@martinrost.de>
2022-01-27 14:25:49 +0100

    added locale and initialIndex

--- lib/paged_vertical_calendar.dart
+++ lib/paged_vertical_calendar.dart
@@ -36,6 +36,8 @@ class PagedVerticalCalendar extends StatefulWidget {
     this.physics,
     this.scrollController,
     this.listPadding = EdgeInsets.zero,
+    this.languageCode,
+    this.initialIndex,
   });
 
   /// the [DateTime] to start the calendar from, if no [startDate] is provided
@@ -85,85 +87,190 @@ class PagedVerticalCalendar extends StatefulWidget {
 
   /// scroll controller for making programmable scroll interactions
   final ScrollController? scrollController;
+  
+  /// Language Code String
+  final String languageCode;
+
+  /// init with this index
+  final int initialIndex;
 
   @override
   _PagedVerticalCalendarState createState() => _PagedVerticalCalendarState();
 }
 
 class _PagedVerticalCalendarState extends State<PagedVerticalCalendar> {
-  late PagingController<int, Month> controller;
+  late PagingController<int, Month> _pagingReplyUpController;
+  late PagingController<int, Month> _pagingReplyDownController;
 
   @override
   void initState() {
     super.initState();
-    controller = PagingController<int, Month>(
+    _pagingReplyUpController = PagingController<int, Month>(
       firstPageKey: 0,
       invisibleItemsThreshold: widget.invisibleMonthsThreshold,
     );
-    controller.addPageRequestListener(fetchItems);
-    controller.addStatusListener(paginationStatus);
+    _pagingReplyUpController.addPageRequestListener(_fetchUpPage);
+    _pagingReplyUpController.addStatusListener(paginationStatusUp);
+
+    _pagingReplyDownController = PagingController<int, Month>(
+      firstPageKey: 0,
+      invisibleItemsThreshold: widget.invisibleMonthsThreshold,
+    );
+    _pagingReplyDownController.addPageRequestListener(_fetchDownPage);
+    _pagingReplyDownController.addStatusListener(paginationStatusDown);
   }
 
-  void paginationStatus(PagingStatus state) {
+  void paginationStatusUp(PagingStatus state) {
+    //print(PagingStatus);
     if (state == PagingStatus.completed)
-      return widget.onPaginationCompleted?.call();
+      return widget.onPaginationCompleted.call();
+  }
+
+  void paginationStatusDown(PagingStatus state) {
+    //print(PagingStatus);
+    if (state == PagingStatus.completed)
+      return widget.onPaginationCompleted.call();
   }
 
   /// fetch a new [Month] object based on the [pageKey] which is the Nth month
   /// from the start date
-  void fetchItems(int pageKey) async {
+  void _fetchUpPage(int pageKey) async {
+    print("fetch up: " + pageKey.toString());
+    try {
+      final month = DateUtils.getMonth(
+          DateTime(
+              widget.startDate.year,
+              widget.startDate.month + widget.initialIndex,
+              widget.startDate.day),
+          widget.endDate,
+          pageKey + 1,
+          true);
+
+      WidgetsBinding.instance?.addPostFrameCallback(
+        (_) => widget.onMonthLoaded.call(month.year, month.month),
+      );
+
+      final newItems = [month];
+      final isLastPage = widget.startDate != null &&
+          widget.startDate.isSameDayOrAfter(month.weeks.first.firstDay);
+
+      if (isLastPage) {
+        return _pagingReplyUpController.appendLastPage(newItems);
+      }
+
+      final nextPageKey = pageKey + newItems.length;
+      _pagingReplyUpController.appendPage(newItems, nextPageKey);
+    } catch (_) {
+      _pagingReplyUpController.error;
+    }
+  }
+
+  void _fetchDownPage(int pageKey) async {
+    print("fetch down: " + pageKey.toString());
     try {
       final month = DateUtils.getMonth(
         widget.startDate,
         widget.endDate,
-        pageKey,
+        pageKey + widget.initialIndex,
+        false,
       );
 
       WidgetsBinding.instance?.addPostFrameCallback(
-        (_) => widget.onMonthLoaded?.call(month.year, month.month),
+        (_) => widget.onMonthLoaded.call(month.year, month.month),
       );
 
       final newItems = [month];
       final isLastPage = widget.endDate != null &&
-          widget.endDate!.isSameDayOrBefore(month.weeks.last.lastDay);
+          widget.endDate.isSameDayOrBefore(month.weeks.last.lastDay);
 
-      if (isLastPage) return controller.appendLastPage(newItems);
+      if (isLastPage) {
+        return _pagingReplyDownController.appendLastPage(newItems);
+      }
 
       final nextPageKey = pageKey + newItems.length;
-      controller.appendPage(newItems, nextPageKey);
+      _pagingReplyDownController.appendPage(newItems, nextPageKey);
     } catch (_) {
-      controller.error;
+      _pagingReplyDownController.error;
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox.expand(
-      child: PagedListView<int, Month>(
-        addAutomaticKeepAlives: widget.addAutomaticKeepAlives,
-        padding: widget.listPadding,
-        pagingController: controller,
-        physics: widget.physics,
-        scrollController: widget.scrollController,
-        builderDelegate: PagedChildBuilderDelegate<Month>(
-          itemBuilder: (BuildContext context, Month month, int index) {
-            return _MonthView(
-              month: month,
-              monthBuilder: widget.monthBuilder,
-              dayBuilder: widget.dayBuilder,
-              onDayPressed: widget.onDayPressed,
-            );
-          },
-        ),
-      ),
+    return Scrollable(
+      viewportBuilder: (BuildContext context, ViewportOffset position) {
+        return Viewport(
+          offset: position,
+          center: downListKey,
+          slivers: [
+            PagedSliverList(
+              pagingController: _pagingReplyUpController,
+              builderDelegate: PagedChildBuilderDelegate<Month>(
+                itemBuilder: (BuildContext context, Month month, int index) {
+                  return _MonthView(
+                    month: month,
+                    monthBuilder: widget.monthBuilder,
+                    dayBuilder: widget.dayBuilder,
+                    onDayPressed: widget.onDayPressed,
+                    languageCode: widget.languageCode,
+                  );
+                },
+              ),
+            ),
+            PagedSliverList(
+              key: downListKey,
+              pagingController: _pagingReplyDownController,
+              builderDelegate: PagedChildBuilderDelegate<Month>(
+                itemBuilder: (BuildContext context, Month month, int index) {
+                  return _MonthView(
+                    month: month,
+                    monthBuilder: widget.monthBuilder,
+                    dayBuilder: widget.dayBuilder,
+                    onDayPressed: widget.onDayPressed,
+                    languageCode: widget.languageCode,
+                  );
+                },
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 
   @override
   void dispose() {
-    controller.dispose();
+    _pagingReplyUpController.dispose();
+    _pagingReplyDownController.dispose();
     super.dispose();
   }
+}
+
+List<String> getDaysOfWeek([String locale = 'en']) {
+  var today = DateTime.now();
+
+  while (today.weekday != DateTime.monday) {
+    today = today.subtract(const Duration(days: 1));
+  }
+  final dateFormat = DateFormat(DateFormat.ABBR_WEEKDAY, locale);
+  final daysOfWeek = [
+    dateFormat.format(today),
+    dateFormat.format(today.add(const Duration(days: 1))),
+    dateFormat.format(today.add(const Duration(days: 2))),
+    dateFormat.format(today.add(const Duration(days: 3))),
+    dateFormat.format(today.add(const Duration(days: 4))),
+    dateFormat.format(today.add(const Duration(days: 5))),
+    dateFormat.format(today.add(const Duration(days: 6)))
+  ];
+
+  return daysOfWeek;
+}
+
+Widget _pattern(BuildContext context, String weekday) {
+  return Center(
+    child: Text(
+      weekday.toUpperCase(),
+    ),
+  );
 }
 
 class _MonthView extends StatelessWidget {
@@ -172,25 +279,42 @@ class _MonthView extends StatelessWidget {
     this.monthBuilder,
     this.dayBuilder,
     this.onDayPressed,
+    this.languageCode,
   });
 
   final Month month;
   final MonthBuilder? monthBuilder;
   final DayBuilder? dayBuilder;
   final ValueChanged<DateTime>? onDayPressed;
+  final String languageCode;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         /// display the default month header if none is provided
-        monthBuilder?.call(context, month.month, month.year) ??
-            _DefaultMonthView(month: month.month, year: month.year),
-
+        monthBuilder.call(context, month.month, month.year) ??
+            _DefaultMonthView(
+              month: month.month,
+              year: month.year,
+              languageCode: languageCode,
+            ),
+        GridView.count(
+            crossAxisCount: DateTime.daysPerWeek,
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            padding: EdgeInsets.zero,
+            children: List.generate(DateTime.daysPerWeek, (index) {
+              final weekDay = getDaysOfWeek(languageCode)[index];
+              return _pattern(context, weekDay);
+            })),
         Table(
           children: month.weeks.map((Week week) {
             return _generateWeekRow(context, week);
           }).toList(growable: false),
+        ),
+        SizedBox(
+          height: 20,
         ),
       ],
     );
@@ -232,15 +356,16 @@ class _MonthView extends StatelessWidget {
 class _DefaultMonthView extends StatelessWidget {
   final int month;
   final int year;
+  final String languageCode;
 
-  _DefaultMonthView({required this.month, required this.year});
+  _DefaultMonthView({required this.month, required this.year, this.languageCode});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Text(
-        DateFormat('MMMM yyyy').format(DateTime(year, month)),
+        DateFormat.yMMMM(languageCode).format(DateTime(year, month)),
         style: Theme.of(context).textTheme.headline6,
       ),
     );
