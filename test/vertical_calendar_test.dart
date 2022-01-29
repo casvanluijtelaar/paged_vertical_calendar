ae511729b7c956eff159c1666befd3caa9a5535c
Martin Rost <martin.rost@5w-verlag.de>
2022-01-29 16:36:00 +0100

    fix of minor issues

--- test/vertical_calendar_test.dart
+++ test/vertical_calendar_test.dart
@@ -6,7 +6,7 @@ import 'package:paged_vertical_calendar/utils/date_utils.dart';
 void main() {
   group('utils', () {
     test('get a month without provided dates', () {
-      final month = DateUtils.getMonth(null, null, 0);
+      final month = DateUtils.getMonth(null, null, 0, true);
       expect(month, isNotNull);
       expect(month.month, DateTime.now().month);
       expect(month.daysInMonth,
@@ -14,7 +14,7 @@ void main() {
     });
 
     test('get a month with provided start date', () {
-      final month = DateUtils.getMonth(DateTime(2020, 1, 1), null, 0);
+      final month = DateUtils.getMonth(DateTime(2020, 1, 1), null, 0, true);
       expect(month, isNotNull);
       expect(month.month, 1);
       expect(month.daysInMonth,
@@ -23,8 +23,8 @@ void main() {
     });
 
     test('get a month with provided end date', () {
-      final month =
-          DateUtils.getMonth(DateTime(2020, 1, 1), DateTime(2020, 5, 1), 4);
+      final month = DateUtils.getMonth(
+          DateTime(2020, 1, 1), DateTime(2020, 5, 1), 4, true);
       expect(month, isNotNull);
       expect(month.month, 5);
       expect(month.daysInMonth,
